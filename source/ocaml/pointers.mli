871ea986409c3ecc57869351e27ff3dda1f36948
U-APCA2\Franck Binard <Franck Binard@BNO-L-PC0ANN8B.APCA2.GC.CA>
2019-05-17 12:59:37 -0400

    reorg of repo

--- source/ocaml/pointers.mli
+++
@@ -1,6 +0,0 @@
-open Syntax
-
-(***********************)
-val complexity_of_expr : t_Expr->int
-val hash : t_Expr->int->int
-
--- source/ocaml/test_cases.mli
+++
@@ -1,7 +0,0 @@
-open Syntax
-open Binding
-
-type test_case = term list * term
-val empty_test_case : test_case
-val read_test_cases : string->context->(test_case list * ty)
-val match_case : ?debug:bool -> term ->context ->test_case -> bool
